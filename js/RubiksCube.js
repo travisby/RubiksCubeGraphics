05d8a5097c179a1af67728e4dbe9e8ccecaab80e
Travis Beatty <travisby@gmail.com>
2013-10-28 15:29:20 -0400

    Fixed Cube functions

--- js/RubiksCube.js
+++ js/RubiksCube.js
@@ -3,15 +3,38 @@
  * @constructor
  */
 function RubiksCube() {
+  /**
+   *
+   * @type {Cubie[]}
+   */
+  var cubies = [];
+
+  // Create our cubies
+  for (var i = 0; i <= 27; i++) {
+    cubies[i] = new Cubie();
+  }
+
+  this._getCubies = function() {
+    return cubies;
+  }
 
 }
 
+/**
+ * Facial representation of our cube in order... U, D, F, B, L, R
+ * @returns {Face[]}
+ */
 RubiksCube.prototype.getFaces = function() {
-  // TODO
+  // TODO actually faces
+  return [new Face(), new Face(), new Face(), new Face(), new Face(), new Face()];
 }
 
 RubiksCube.prototype.draw = function() {
-  this.getFaces().draw();
+  var faces = this.getFaces();
+
+  for (var i = 0; i < faces.length; i++) {
+    faces[i].draw();
+  }
 }
 /**
  * Builds a RubiksCube by using a file handle to get the colors
@@ -20,6 +43,8 @@ RubiksCube.prototype.draw = function() {
  * @constructor
  */
 function RubiksCubeFactoryFromFile(file) {
-    // TODO build from file
-    return new RubiksCube();
+  // TODO build from file
+  var cube = new RubiksCube();
+
+  return cube;
 }
