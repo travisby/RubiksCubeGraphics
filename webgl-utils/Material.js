88fb3c747f01ae93c1b7aca54c6d044c0da6f49a
Jacob Leach <leach.jacob@gmail.com>
2013-10-28 13:12:08 -0400

    Forgot to add Material.js

---
+++ webgl-utils/Material.js
@@ -0,0 +1,6 @@
+function Material(ambient, diffuse, specular, shininess) {
+    this.ambient = ambient;
+    this.diffuse = diffuse;
+    this.specular = specular;
+    this.shininess = shininess;
+}
