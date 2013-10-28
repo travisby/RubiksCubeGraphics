c46ee5f9681e0446c5cfe2fd17a5f68445ffa9df
Travis Beatty <travisby@gmail.com>
2013-10-28 14:55:26 -0400

    Updated how we handle colors

---
+++ js/Color.js
@@ -0,0 +1,34 @@
+function Color() {
+}
+
+function White() {
+}
+White.prototype = new Color();
+
+function Red() {
+}
+Red.prototype = new Color();
+
+function Blue() {
+}
+Blue.prototype = new Color();
+
+function Orange() {
+}
+Orange.prototype = new Color();
+
+function Green() {
+}
+Green.prototype = new Color();
+
+function Yellow() {
+}
+Yellow.prototype = new Color();
+
+NOCOLOR = new Color();
+WHITE = new White();
+RED = new Red();
+BLUE = new Blue();
+ORANGE = new Orange();
+GREEN = new Green();
+YELLOW = new Yellow();
