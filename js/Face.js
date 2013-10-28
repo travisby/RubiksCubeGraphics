d72872ca2a752a376f09e1a453d0816665819ec8
Travis Beatty <travisby@gmail.com>
2013-10-28 16:03:17 -0400

    Added Face types for direction handling

--- js/Face.js
+++ js/Face.js
@@ -20,3 +20,40 @@ Face.prototype.turn = function() {
 Face.prototype.draw = function() {
   // TODO
 }
+
+// B, L, D, R, F, U
+function BackFace(cubies) {
+  Face.apply(this, cubies);
+}
+BackFace.prototype = new Face();
+BackFace.prototype.constructor = Face;
+
+function LeftFace(cubies) {
+  Face.apply(this, cubies);
+}
+LeftFace.prototype = new Face();
+LeftFace.prototype.constructor = Face;
+
+function RightFace(cubies) {
+  Face.apply(this, cubies);
+}
+RightFace.prototype = new Face();
+RightFace.prototype.constructor = Face;
+
+function FrontFace(cubies) {
+  Face.apply(this, cubies);
+}
+FrontFace.prototype = new Face();
+FrontFace.prototype.constructor = Face;
+
+function DownFace(cubies) {
+  Face.apply(this, cubies);
+}
+DownFace.prototype = new Face();
+DownFace.prototype.constructor = Face;
+
+function UpFace(cubies) {
+  Face.apply(this, cubies);
+}
+UpFace.prototype = new Face();
+UpFace.prototype.constructor = Face;
--- js/RubiksCube.js
+++ js/RubiksCube.js
@@ -28,12 +28,12 @@ RubiksCube.prototype.getFaces = function() {
   var cubies = this._getCubies();
 
   return [
-      new Face([cubies[0], cubies[1], cubies[2], cubies[3], cubies[4], cubies[5],cubies[6], cubies[7], cubies[8]]),
-      new Face([cubies[3], cubies[5], cubies[8], cubies[9], cubies[11], cubies[13], cubies[10], cubies[12], cubies[14]]),
-      new Face([cubies[8], cubies[7], cubies[6], cubies[13], cubies[16], cubies[15], cubies[14], cubies[18], cubies[17]]),
-      new Face([cubies[0], cubies[3], cubies[6], cubies[20], cubies[22], cubies[15], cubies[19], cubies[21], cubies[17]]),
-      new Face([cubies[14], cubies[28], cubies[17], cubies[12], cubies[24], cubies[21], cubies[10], cubies[23], cubies[19]]),
-      new Face([cubies[2], cubies[1], cubies[0], cubies[9], cubies[25], cubies[20], cubies[11], cubies[23], cubies[19]])
+      new BackFace([cubies[0], cubies[1], cubies[2], cubies[3], cubies[4], cubies[5],cubies[6], cubies[7], cubies[8]]),
+      new LeftFace([cubies[3], cubies[5], cubies[8], cubies[9], cubies[11], cubies[13], cubies[10], cubies[12], cubies[14]]),
+      new DownFace([cubies[8], cubies[7], cubies[6], cubies[13], cubies[16], cubies[15], cubies[14], cubies[18], cubies[17]]),
+      new RightFace([cubies[0], cubies[3], cubies[6], cubies[20], cubies[22], cubies[15], cubies[19], cubies[21], cubies[17]]),
+      new FrontFace([cubies[14], cubies[28], cubies[17], cubies[12], cubies[24], cubies[21], cubies[10], cubies[23], cubies[19]]),
+      new UpFace([cubies[2], cubies[1], cubies[0], cubies[9], cubies[25], cubies[20], cubies[11], cubies[23], cubies[19]])
   ];
 }
 
