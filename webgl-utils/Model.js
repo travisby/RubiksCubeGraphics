a63462e11cc8aa3301dfcded6279d93b9b8f8af6
Jacob Leach <leach.jacob@gmail.com>
2013-10-28 21:03:33 -0400

    Model is done I think...

--- webgl-utils/Model.js
+++ webgl-utils/Model.js
@@ -1,3 +1,13 @@
 var Model = function() {
+    this.shapes = [];
+}
 
+Model.prototype.add = function(shape) {
+    this.shapes.push(shape);
+}
+
+Model.prototype.draw = function(gl, shader, light, camera) {
+    for(var i in this.shapes) {
+        this.shapes[i].draw(gl, shader, light, camera);
+    }
 }
