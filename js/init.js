63b70d0cf9b59b9932920570478dc06d2ad31b22
Jacob Leach <leach.jacob@gmail.com>
2013-10-28 21:50:43 -0400

    Whatever I've changed

--- js/init.js
+++ js/init.js
@@ -1,6 +1,3 @@
-var drawables = [];
-var gl;
-
 window.onload = function() {
     var BLACK = [0.0, 0.0, 0.0, 1.0];
     var WHITE = [1.0, 1.0, 1.0, 1.0];
@@ -9,38 +6,29 @@ window.onload = function() {
     var GREEN = [0.0, 1.0, 0.0, 1.0];
     var ORANGE = [1.0, 0.498, 0.0, 1.0];
     var YELLOW = [1.0, 1.0, 0.0, 1.0];
-    var colors = [BLACK, BLUE, RED, GREEN, ORANGE, YELLOW];
+    var colors = [WHITE, BLUE, RED, GREEN, ORANGE, YELLOW];
 
     var camera = new Camera();
     camera.setOrtho(-5,5,-5,5,-5,5);
-    camera.move(2,2,2);
-    camera.aim(0,1,0);
-    camera.direction(2,2,2);
+    camera.move(2,1,2);
+    camera.aim(0,0,0);
+    camera.direction(2,6,2);
     
     var light = new Light();
-    light.setPosition(2,2,2,1);
-    light.setSpecular(1,.5,1,.1);
-    light.setDiffuse(.1,1,.8,.5);
-    light.setAmbient(.1,.1,.1,.5);
+    light.setPosition(10,10,10,1);
+    light.setSpecular(1,1,1,1);
+    light.setDiffuse(.2,.2,.2,1);
+    light.setAmbient(.2,.2,.2,.1);
     
-    var cube = new SolidCube(new Material(new vec4(1,1,1,.1), vec4(1,1,1,.1), vec4(1,1,1,.1), .1), colors); 
-    var cube1 = new SolidCube(new Material(new vec4(1,1,1,.1), vec4(1,1,1,.1), vec4(1,1,1,.1), .1), colors);
+    var cube = new SolidCube(new Material(new vec4(1,1,1,.1), vec4(1,1,1,.1), vec4(1,1,1,.1), 1000), colors); 
+    var cube1 = new SolidCube(new Material(new vec4(1,1,1,.1), vec4(1,1,1,.1), vec4(1,1,1,.1), 1000), colors);
     
     var scene = new Scene("cubeCanvas", light, camera);
     scene.add(cube);
     scene.add(cube1);   
     
     cube1.move(4,1); 
-    cube.move(3,1);
+    cube.move(-1,1);
     
     scene.render(); 
 }
-
-var render = function(){
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
-    for ( var i in drawables ) {
-        drawables[i].draw();
-    }       
-
-    requestAnimFrame(render); 
-}   
