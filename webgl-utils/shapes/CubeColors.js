fca6e14d5cc8acde943d17b6fed1560fae2a9dc3
Jacob Leach <leach.jacob@gmail.com>
2013-10-30 09:45:22 -0400

    The constants were a bit wrong

--- webgl-utils/shapes/CubeColors.js
+++ webgl-utils/shapes/CubeColors.js
@@ -1,22 +1,23 @@
-const CUBE_FRONT    = 0;
+const CUBE_FRONT    = 4;
 const CUBE_LEFT     = 1;
 const CUBE_TOP      = 2;
 const CUBE_RIGHT    = 3;
-const CUBE_BACK     = 4;
+const CUBE_BACK     = 0;
 const CUBE_BOTTOM   = 5;
 
 var CubeColors = function() {
     this.colors = [];
     this.reset();
 }
-CubeColors.prototype.FACE_CONSTANTS = [CUBE_FRONT, CUBE_LEFT, CUBE_TOP, CUBE_RIGHT, CUBE_BACK, CUBE_BOTTOM];
+
+CubeColors.prototype.FACE_CONSTANTS = [CUBE_BACK, CUBE_LEFT, CUBE_TOP, CUBE_RIGHT, CUBE_FRONT, CUBE_BOTTOM];
 
 CubeColors.prototype.getColors = function() {
     var myColors = [];
     for (var i = 0; i < this.FACE_CONSTANTS.length; i++) {
-      myColors[i] = [this.colors[i].r, this.colors[i].b, this.colors[i].g, this.colors[i].alpha];
+        myColors[i] = [this.colors[i].r, this.colors[i].b, this.colors[i].g, this.colors[i].alpha];
     }
-    return this.colors;
+    return myColors;
 }
 
 CubeColors.prototype.set = function(face, color) {
