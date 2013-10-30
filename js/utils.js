142f7dfa0e10f58cfea732ac0f483c5ac2a92168
Travis Beatty <travisby@gmail.com>
2013-10-29 22:14:24 -0400

    Made fileToString async

--- js/utils.js
+++ js/utils.js
@@ -1,21 +1,13 @@
 /**
- * Builds a string from a file... blocking
- * @param {File} file
- * @returns {string}
+ * Builds a string from a file... then calls a callback
+ * @param file
+ * @param callback
  */
-function fileToString(file) {
+function fileToString(file, callback) {
     // https://www.inkling.com/read/javascript-definitive-guide-david-flanagan-6th/chapter-22/reading-text-files-with
     var reader = new FileReader();  // Create a FileReader object
-    var text = '';
 
-    reader.readAsText(file);           // Read the file
-    reader.onload = function() {    // Define an event handler
-        text = reader.result;   // This is the file contents
-    }
+    reader.onloadend = callback;
 
-    while (!text) {
-        // loop while we wait for file reading to end
-    }
-
-    return text;
+    reader.readAsText(file);
 }
