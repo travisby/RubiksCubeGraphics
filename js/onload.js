d598d3810e4ecf0c9ec099bfc87762bd545912bf
Jacob Leach <leach.jacob@gmail.com>
2013-10-30 09:53:19 -0400

    this works

--- js/onload.js
+++ js/onload.js
@@ -1,14 +1,14 @@
 var cameraA;
 var cameraB;
 var rubiksCube;
-
+var light;
 window.onload = function() {
     init();
     cameraA = new Camera();
     cameraA.setOrtho(-5,5,-5,5,-5,5);
-    cameraA.move(2,2,2);
+    cameraA.move(0,0,2);
     cameraA.aim(0,0,0);
-    cameraA.direction(2,3,2);
+    cameraA.direction(0,3,2);
     
     cameraB = new Camera();
     cameraB.setOrtho(-5,5,-5,5,-5,5);
@@ -16,25 +16,24 @@ window.onload = function() {
     cameraB.aim(0,0,0);
     cameraB.direction(-2,3,-2);
     
-    var light = new Light();
-    light.setPosition(10,10,10,1);
-    light.setSpecular(1,1,1,1);
+    light = new Light();
+    light.setPosition(100,100,100,0);
+    light.setSpecular(.5,.5,.5,1);
     light.setDiffuse(1,1,1,1);
-    light.setAmbient(0,.1,.1,1);
+    light.setAmbient(1,1,1,.1);
 
     var sceneA = new Scene("canvasA", light, cameraA);
     var sceneB = new Scene("canvasB", light, cameraB); 
-    var rubiksCube = RubiksCubeFactoryFromString("GGWRRGRRGOWWGGOYYROGOYYYRBRYYYRBGRWWBOYBOBBOBOGOWWBWWB"); 
+    //var rubiksCube = RubiksCubeFactoryFromString("GGWRRGRRGOWWGGOYYROGOYYYRBRYYYRBGRWWBOYBOBBOBOGOWWBWWB"); 
     
     var colors = new CubeColors();
-    
     colors.set(CUBE_TOP, WHITE);
     colors.set(CUBE_BOTTOM, YELLOW);
     colors.set(CUBE_LEFT, ORANGE);
     colors.set(CUBE_RIGHT, RED);
     colors.set(CUBE_FRONT, BLUE);
     colors.set(CUBE_BACK, GREEN);
-    rubiksCube = new SolidCube(new Material(vec4(1,1,1,1), vec4(1,1,1,1), vec4(.001,.001,.001,.1), 100000), colors);
+    rubiksCube = new SolidCube(new Material(vec4(0,0,0,1), vec4(0,0,0,1), vec4(0,0,0,1), 10000), colors);
     
     sceneA.add(rubiksCube);
     sceneB.add(rubiksCube);
